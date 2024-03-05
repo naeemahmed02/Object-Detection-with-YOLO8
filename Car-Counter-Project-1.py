01d1ddde75ad86c2f00c5c11812ba0ea07dbe854
Naeem Ahmed <naeemahmedbdn2002@gmail.com>
2024-03-05 18:04:19 +0500

    Repo Updated

--- Car-Counter-Project-1.py
+++ Car-Counter-Project-1.py
@@ -5,15 +5,19 @@ import cvzone
 import math
 
 ######### Source
-cap = cv.VideoCapture('resources/bikes.mp4')
-
+cap = cv.VideoCapture('resources/cars.mp4')
+# cap.set(3,980)
+# cap.set(4,450)
+mask = cv.imread('resources/mask.png')
+mask = cv.resize(mask, (1280, 720))
 ######## Define the model
 model = YOLO('wieghts/yolov8n.pt')
 
 ######## Looping the Source Frame by Frame
 while True:
     _, img = cap.read()
-    results = model(img, stream=True)
+    image_region = cv.bitwise_and(img, mask)
+    results = model(image_region, stream=True)
     for r in results:
         boxes = r.boxes
         for box in boxes:
@@ -23,10 +27,11 @@ while True:
             class_name = model.names[cls]   #### Using class index getting the class name
             conf = math.ceil((box.conf[0] * 100))/100   #### Getting Predicted Confidence
             w, h = x2 - x1, y2 - y1
-            cvzone.cornerRect(img, bbox=(x1, y1, w, h))
-            cvzone.putTextRect(img, f"{class_name}{' '}{conf}", (max(0,x1), max(35, y1)), scale=0.7, thickness=1, offset=3)
+            if class_name == "car" and conf >=0.35:
+                cvzone.cornerRect(image_region, bbox=(x1, y1, w, h))
+                cvzone.putTextRect(image_region, f"{class_name}{' '}{conf}", (max(0,x1), max(35, y1)), scale=0.7, thickness=1, offset=3)
 
-    cv.imshow("Car Counter", img)
+    cv.imshow("Mask", image_region)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
